936971748b29d2041b9153e44949755c68a71c07
Dillon Loh <loh.dillon@gmail.com>
2024-06-15 01:47:01 +0800

    chore: linting

--- stream_pitch_detector/audio_processing/audio_processor.py
+++ stream_pitch_detector/audio_processing/audio_processor.py
@@ -4,9 +4,17 @@ import numpy as np
 
 class AudioProcessor:
     """Class for processing audio chunks"""
-    def __init__(self, buffer_size: int = 512, hop_size: int = 512, sample_rate: int = 44100, unit: str = "Hz", tolerance: float = 0.8):
+
+    def __init__(
+        self,
+        buffer_size: int = 512,
+        hop_size: int = 512,
+        sample_rate: int = 44100,
+        unit: str = "Hz",
+        tolerance: float = 0.8,
+    ):
         """Initialise audio processor
-        
+
         Args:
             buffer_size (int, optional): The buffer size. Defaults to 1024.
             hop_size (int, optional): The hop size. Defaults to 512.
@@ -16,19 +24,21 @@ class AudioProcessor:
         self.buffer_size = buffer_size
         self.hop_size = hop_size
         self.sample_rate = sample_rate
-        self.pitch_detector = aubio.pitch("default", self.buffer_size, self.hop_size, self.sample_rate)
+        self.pitch_detector = aubio.pitch(
+            "default", self.buffer_size, self.hop_size, self.sample_rate
+        )
         self.pitch_detector.set_unit(unit)
         self.pitch_detector.set_tolerance(tolerance)
 
     def get_chunk_pitch(self, chunk: np.ndarray) -> float:
         """Get the pitch of an audio chunk
-        
+
         Args:
             chunk (np.ndarray): The audio chunk
-        
+
         Returns:
             float: The pitch of the audio chunk
-        
+
         """
 
         # aubio requires float32 input
@@ -38,10 +48,8 @@ class AudioProcessor:
         if chunk.shape[1] == 2:
             # convert to mono by averaging the channels for each sample
             chunk = np.mean(chunk, axis=1)
-            
+
         # Get the pitch from the chunk
         pitch = self.pitch_detector(chunk)[0]
-        
-        return pitch
 
-        
+        return pitch
--- stream_pitch_detector/audio_streaming/linux_audio_stream.py
+++ stream_pitch_detector/audio_streaming/linux_audio_stream.py
@@ -1,14 +1,19 @@
 import subprocess
 
-import aubio
 import numpy as np
 
 from audio_streaming.audio_stream import AudioStream
 
+
 class LinuxAudioStream(AudioStream):
-    
-    def __init__(self, os: str = "linux", chunk_size: int = 512, 
-                 rate: int = 44100, channels: int = 2, format: np.dtype = np.int16):
+    def __init__(
+        self,
+        os: str = "linux",
+        chunk_size: int = 512,
+        rate: int = 44100,
+        channels: int = 2,
+        format: np.dtype = np.int16,
+    ):
         self.os = "linux"
         self.chunk_size = chunk_size
         self.rate = rate
@@ -17,75 +22,85 @@ class LinuxAudioStream(AudioStream):
         self.device = self._get_default_sink_monitor()
 
         self.parec_command = [
-            'parec',
-            '-d', self.device,
-            '--rate', str(self.rate),
-            '--channels', str(self.channels),
-            '--format', 's16le'
+            "parec",
+            "-d",
+            self.device,
+            "--rate",
+            str(self.rate),
+            "--channels",
+            str(self.channels),
+            "--format",
+            "s16le",
         ]
 
         self.stream_process = None
 
     def _get_default_sink_monitor(self) -> str:
-        """ 
-        Get the monitor source of the default sink 
-        
+        """
+        Get the monitor source of the default sink.
+        A monitor source is a virtual input source that represents the audio output of a sink.
+        Since parec requires an input source to record audio, we use the monitor source of the default sink.
+
         Returns:
             str: The monitor source of the default sink
 
         Raises:
             Warning: If the monitor source for the default sink could not be found
         """
-        default_sink = subprocess.run(['pactl', 'get-default-sink'], capture_output=True, text=True).stdout.strip()
-        sinks = subprocess.run(['pactl', 'list', 'short', 'sinks'], capture_output=True, text=True).stdout.splitlines()
+        default_sink = subprocess.run(
+            ["pactl", "get-default-sink"], capture_output=True, text=True
+        ).stdout.strip()
+        sinks = subprocess.run(
+            ["pactl", "list", "short", "sinks"], capture_output=True, text=True
+        ).stdout.splitlines()
         for sink in sinks:
             parts = sink.split()
             if parts[1] == default_sink:
                 monitor_source = parts[1] + ".monitor"
                 return monitor_source
-            
+
         raise Warning("Could not find the monitor source for the default sink.")
-    
+
     def start_stream(self):
         """
         Start the audio stream
-        
+
         """
         if not self.stream_process:
-            self.stream_process = subprocess.Popen(self.parec_command, stdout=subprocess.PIPE, bufsize=self.chunk_size)
-        
+            self.stream_process = subprocess.Popen(
+                self.parec_command, stdout=subprocess.PIPE, bufsize=self.chunk_size
+            )
+
         else:
             raise Warning("Stream is already running")
-        
+
     def stop_stream(self):
         """
         Stop the audio stream
-        
+
         """
         if self.stream_process:
             self.stream_process.terminate()
             self.stream_process = None
-        
-        else:    
+
+        else:
             raise Exception("Stream is not running")
-    
 
     def stream_audio_chunk(self) -> np.ndarray:
         """
         Return a chunk of audio data from the stream
-        
+
         Returns:
             np.ndarray: The audio data chunk
-        
+
         """
         if not self.stream_process:
             raise Exception("Stream is not running")
 
         chunk_bin = self.stream_process.stdout.read(self.chunk_size * self.channels * 2)
-        
+
         # reshape the data to the correct shape [chunk_size, channels]
         # works since the data from the stereo channels is interleaved
         chunk = np.frombuffer(chunk_bin, dtype=self.format).reshape(-1, self.channels)
 
         return chunk
-    
