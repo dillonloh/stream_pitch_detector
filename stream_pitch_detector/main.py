bf6338d0c5bda896410e76bf78f5f434ed4253a4
Dillon Loh <loh.dillon@gmail.com>
2024-06-15 21:51:42 +0800

    refactor: params in main

--- stream_pitch_detector/main.py
+++ stream_pitch_detector/main.py
@@ -7,33 +7,40 @@ from scipy.io.wavfile import write
 from audio_streaming.linux_audio_stream import LinuxAudioStream
 from audio_processing.audio_processor import AudioProcessor
 
-OUTPUT_FILE = "shifted_audio.wav"
-SEMITONES = -4
-DURATION = 60
 
-
-def main():
+def main(
+    chunk_size=2048,
+    sample_rate=44100,
+    semitones=0,
+    duration=-1,
+    output_file="shifted_audio.wav",
+):
     # check if os is linux
     try:
         current_platform = platform.system().lower()
         if "linux" in current_platform:
-            audio_stream = LinuxAudioStream()
+            audio_stream = LinuxAudioStream(chunk_size=chunk_size, rate=sample_rate)
         else:
             raise NotImplementedError(
                 f"OS: {current_platform} is not currently supported"
             )
 
-        audio_processor = AudioProcessor()
+        audio_processor = AudioProcessor(
+            buffer_size=chunk_size, hop_size=chunk_size, sample_rate=sample_rate
+        )
         audio_stream.start_stream()
 
         shifted_audio = []
         start_time = time.time()
 
-        while (time.time() - start_time) < DURATION:
+        if duration < 0:  # if duration is not explicitly set, record forever
+            duration = float("inf")
+
+        while (time.time() - start_time) < duration:
             chunk = audio_stream.stream_audio_chunk()
             pitch = audio_processor.get_chunk_pitch(chunk)
             print(f"Pitch: {pitch}")
-            shifted_chunk = audio_processor.shift_pitch(chunk, semitones=SEMITONES)
+            shifted_chunk = audio_processor.shift_pitch(chunk, semitones=semitones)
             shifted_audio.append(shifted_chunk)
 
     except KeyboardInterrupt:
@@ -49,8 +56,14 @@ def main():
     shifted_audio = np.concatenate(shifted_audio, axis=0)
 
     # Save the shifted audio to a file
-    write(OUTPUT_FILE, audio_stream.rate, shifted_audio.astype(np.int16))
+    write(output_file, audio_stream.rate, shifted_audio.astype(np.int16))
 
 
 if __name__ == "__main__":
-    main()
+    main(
+        chunk_size=2048,
+        sample_rate=44100,
+        semitones=-4,
+        duration=60,
+        output_file="shifted_audio.wav",
+    )
